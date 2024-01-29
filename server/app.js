f867d352e089170d1ccd9c3ec1b8911119e0fcad
SNelson723 <stephenn649@gmail.com>
2024-01-29 14:17:39 -0600

    Added a sync command for syncing the MySQL database, added a post and get request handler server-side for creating and getting all characters

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const { db, User, Skills, Stats, Cantrips, Spells } = require('./db/index');
 // require express
 const express = require('express');
 // require sequelize
@@ -12,10 +13,37 @@ const clientPath = path.resolve(__dirname, '../client/dist');
 
 // setting up the server
 const app = express();
-app.use(cors);
+// make sure you invoke cors() otherwise there will be a socket hangup
+app.use(cors());
 // using a json parser middleware
 app.use(express.json());
 // serve up static files from the client path
 app.use(express.static(clientPath));
+
+app.get('/api/users', (req, res) => {
+  User.findAll()
+    .then((data) => {
+      res.status(200).send(data)
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post('/api/users', (req, res) => {
+  const { character, level, race, alignment, exp } = req.body;
+  User.create({
+    character: character,
+    level: level,
+    race: race,
+    alignment: alignment,
+    experience: exp,
+  })
+    .then(newUser => {
+      console.log(newUser, 'Created successfully');
+      res.sendStatus(201);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
 
 module.exports = app;
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,9 +1,2 @@
 const { db } = require('./index');
 
-db.sync()
-  .then(() => {
-    console.log('Database synchronized');
-  })
-  .catch((err) => {
-    console.error('Database synchronization error: ', err);
-  });
