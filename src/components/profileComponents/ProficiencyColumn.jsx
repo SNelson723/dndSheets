12078696ad342adf5212aca51b47215de36401c8
SNelson723 <stephenn649@gmail.com>
2024-06-07 20:01:54 -0500

    Updated proficiency bonus to a state variable so I can handle all proficiency data on the client side

--- src/components/profileComponents/ProficiencyColumn.jsx
+++ src/components/profileComponents/ProficiencyColumn.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { setProficiencyMod } from "../../modifierFunctions";
 
 const ProficiencyColumn = ({ level }) => {
+  const [profBonus, setProfBonus] = useState('');
+
+  useEffect(() => {
+    setProfBonus(setProficiencyMod(level))
+  });
+
   return (
     <div>
       <div style={{display: 'flex'}}>
-        <p className="text-center pr-2">{setProficiencyMod(level)}</p>
+        <p className="text-center pr-2">{profBonus}</p>
         <label>Proficiency Bonus</label>
       </div>
     </div>
