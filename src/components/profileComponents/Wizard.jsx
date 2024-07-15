ed5386b4c1ff7eebed31435829fc887da7c7c4f5
SNelson723 <stephenn649@gmail.com>
2024-07-14 19:50:25 -0500

    Fixed my faulty on hover logic for the sub info menu and setup the on click events for them to render each relevant component individually

--- src/components/profileComponents/Wizard.jsx
+++ src/components/profileComponents/Wizard.jsx
@@ -1,26 +1,10 @@
 import React,  { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Modal } from 'react-bootstrap';
 import SpellsCantrips from './wizardComponents/SpellsCantrips';
 
 const Wizard = ({userId}) => {
-  const [cantrips, setCantrips] = useState([]);
-  const [spells, setSpells] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [currentHover, setCurrentHover] = useState('');
   const [currentInfoTab, setCurrentInfoTab] = useState('Actions');
-  const level = 1;
-
-  useEffect(() => {
-    // Getting the cantrips by user's id
-    axios.get(`cantrips/${userId}`)
-      .then(({ data }) => setCantrips(Object.values(data)))
-      .catch(err => console.error(err));
-
-    axios.get(`userSpells/${userId}`)
-      .then(({data}) => setSpells(Object.values(data)))
-      .catch(err => console.error(err));
-  }, []);
 
   const toggleModal = (query) => {
     // figure out how to pass in the spell or cantrip to get with axios from external api route
@@ -37,51 +21,45 @@ const Wizard = ({userId}) => {
 
       <div id="userCantrips" className="mt-1" style={{display: 'flex', justifyContent: 'space-evenly'}}>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('Actions')}
-          className={`${currentHover === 'Actions' && 'currentInfoHover'} ${currentInfoTab === 'Actions' && 'currentInfoTab'}`}
+          onClick={() => setCurrentInfoTab('Actions')}
+          className={`subInfoHeader ${currentInfoTab === 'Actions' && 'currentInfoTab'}`}
         >
           Actions
         </h6>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('Spells')}
-          onClick={() => setCurrentInfoTab('Actions')}
-          className={`${currentHover === 'Spells' && 'currentInfoHover'} ${currentInfoTab === 'Spells' && 'currentInfoTab'}`}
+          onClick={() => setCurrentInfoTab('Spells')}
+          className={`subInfoHeader ${currentInfoTab === 'Spells' && 'currentInfoTab'}`}
         >
           Spells
         </h6>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('Inventory')}
-          className={`${currentHover === 'Inventory' && 'currentInfoHover'} ${currentInfoTab === 'Inventory' && 'currentInfoTab'}`}
+        onClick={() => setCurrentInfoTab('Inventory')}
+          className={`subInfoHeader ${currentInfoTab === 'Inventory' && 'currentInfoTab'}`}
         >
           Inventory
         </h6>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('FeatsTraits')}
-          className={`${currentHover === 'FeatsTraits' && 'currentInfoHover'} ${currentInfoTab === 'FeatsTraits' && 'currentInfoTab'}`}
+        onClick={() => setCurrentInfoTab('FeatsTraits')}
+          className={`subInfoHeader ${currentInfoTab === 'FeatsTraits' && 'currentInfoTab'}`}
         >
           Feats/Traits
         </h6>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('Description')}
-          className={`${currentHover === 'Description' && 'currentInfoHover'} ${currentInfoTab === 'Description' && 'currentInfoTab'}`}
+        onClick={() => setCurrentInfoTab('Description')}
+          className={`subInfoHeader ${currentInfoTab === 'Description' && 'currentInfoTab'}`}
         >
           Description
         </h6>
         <h6
-          onMouseLeave={() => setCurrentHover('')}
-          onMouseEnter={() => setCurrentHover('Notes')}
-          className={`${currentHover === 'Notes' && 'currentInfoHover'} ${currentInfoTab === 'Notes' && 'currentInfoTab'}`}
+          onClick={() => setCurrentInfoTab('Notes')}
+          className={`subInfoHeader ${currentInfoTab === 'Notes' && 'currentInfoTab'}`}
         >
           Notes
         </h6>
       </div>
       <div id="subInfoBody">
-        <SpellsCantrips />
+        {/* {currentInfoTab === 'Spells' ? <SpellsCantrips userId={userId} /> : null} */}
+        <SpellsCantrips userId={userId} />
       </div>
     </div>
   );
--- src/components/profileComponents/wizardComponents/SpellsCantrips.jsx
+++ src/components/profileComponents/wizardComponents/SpellsCantrips.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-const SpellsCantrips = () => {
+const SpellsCantrips = ({ userId }) => {
+  const [cantrips, setCantrips] = useState([]);
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    // Getting the cantrips by user's id
+    axios.get(`cantrips/${userId}`)
+      .then(({ data }) => setCantrips(Object.values(data)))
+      .catch(err => console.error(err));
+
+    axios.get(`userSpells/${userId}`)
+      .then(({data}) => setSpells(Object.values(data)))
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log('Cantrips', cantrips);
+  console.log('Spells', spells);
   return (
-    <div>
+    <div id="spellsBody">
       Spells and Cantrips
     </div>
   );
